edf64936c0b413f8ceca8ce8e522372bd41211d7
Alan Wolfe <alan.wolfe@gmail.com>
2022-02-13 16:06:52 -0800

    more progress

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void DFTPoints(const std::vector<float>& points, int minHz, int maxHz, const cha
 {
     FILE* file = nullptr;
     fopen_s(&file, fileName, "wb");
-    fprintf(file, "\"hz\", \"re\", \"im\", \"mag\"\n");
+    fprintf(file, "\"hz\",\"re\",\"im\",\"mag\",\"phase\"\n");
 
     float div = float(points.size());
     for (int hz = minHz; hz <= maxHz; ++hz)
@@ -23,7 +23,7 @@ void DFTPoints(const std::vector<float>& points, int minHz, int maxHz, const cha
             im += sinf(c_twoPi * float(hz) * f) / div;
         }
 
-        fprintf(file, "\"%i\",\"%f\",\"%f\",\"%f\"\n", hz, re, im, sqrtf(re * re + im * im));
+        fprintf(file, "\"%i\",\"%f\",\"%f\",\"%f\",\"%f\"\n", hz, re, im, sqrtf(re * re + im * im), atan2f(im, re));
     }
 
     fclose(file);
@@ -32,6 +32,10 @@ void DFTPoints(const std::vector<float>& points, int minHz, int maxHz, const cha
 int main(int argc, char** argv)
 {
     _mkdir("out");
-    DFTPoints({ 0.0f, 1.0f / 4.0f, 3.0f / 4.0f }, -3, 3, "out/1.csv");
+
+    DFTPoints({ 0.0f, 1.0f / 4.0f, 3.0f / 4.0f }, -10, 10, "out/1.csv");
+
+    DFTPoints({ 0.822120f, 0.547486f, 0.234054f, 0.428338f, 0.78547f, 0.734857f, 0.646972f, 0.941578f, 0.492718f, 0.327577f }, -20, 20, "out/2.csv");
+
     return 0;
 }
